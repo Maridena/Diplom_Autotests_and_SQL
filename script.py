0f7b2e17deb552954d6ee5e184fb03e73837bae0
Maridena <marina.zaborowska@gmail.com>
2024-05-21 16:23:44 +0200

    Create script.py

---
+++ script.py
@@ -0,0 +1,20 @@
+# добавляем модуль requests для выполнения запросов на сервер,
+# модуль data с данными для заказа и модуль с настройками
+
+import requests
+import data
+import configuration
+
+# функция для создания заказа клиентом
+# должна возвращать номер трекера заказа (поле Orders.track в базе)
+def create_order(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER_PATH,
+                  json = body,
+                  headers = data.headers)
+
+# функция для получения заказа по номеру track заказа
+def getOrderByOrderTrack(track):
+    return requests.get(configuration.URL_SERVICE + configuration.GET_ORDER_BY_TRACK + "?t=" + str(track),
+                        headers = data.headers)
+
+
